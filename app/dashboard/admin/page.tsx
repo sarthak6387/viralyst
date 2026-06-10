057fb7659bc7a0dae19d74c3ce7604cf318232ea
Sarthak <sarthakverma6387gmail.com>
2026-06-11 01:20:45 +0530

    Fix admin page module

---
+++ app/dashboard/admin/page.tsx
@@ -0,0 +1,19 @@
+export default function
+AdminPage() {
+
+  return (
+
+    <div className="p-10">
+
+      <h1
+        className="
+          text-5xl
+          font-bold
+        "
+      >
+        Admin Dashboard
+      </h1>
+
+    </div>
+  );
+}
